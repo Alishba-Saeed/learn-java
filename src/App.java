8610d99cceb80a6e2632ca0209eb5142ca60470d
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-08-26 20:01:38 +0500

    Method Overloading

--- src/App.java
+++ src/App.java
@@ -1,34 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-         /* New switch statement
-          * It allow us the expression version -> , when we use this we don't have to explicitly add break
-          * We can use new switch statement to assign values as well which was not possible with the older version
-          * we can return value as well with the new switch statement
-         */
-
-        String day = "Monday";
-        /* It allow us the expression version -> , when we use this we don't have to explicitly add break
-        this will return It's the weekday
-        */
-        switch (day) {
-            case "Saturday, Sunday" -> System.out.println("It's the weekend");
-            default -> System.out.println("It's the weekday");
-        }
-
-        // We can use new switch statement to assign values as well which was not possible with the older version
-        int result;
-        switch (day) {
-            case "Saturday, Sunday" -> result = 0;
-            default -> result = 1;
-        }
-        System.out.println(result);
-
-        // we can return value as well with the new switch statement
-
-        result = switch (day) {
-            case "Saturday, Sunday" -> 0;
-            default -> 1;
-        };
-        System.out.println(result);
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(1, 2));
     }
 }
---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+class Calculator {
+  /*
+   * Method Overloading when we have different number of parameters
+   * we should have the same return type
+   */
+  public int add(int a, int b) {
+    return a+b;
+  }
+
+  public int add(int a, int b, int c) {
+    return a+b+c;
+  }
+}
