03a7006247c864afbabb1ae3667771d12d032ab0
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-08-26 19:10:37 +0500

    New switch statement

--- src/App.java
+++ src/App.java
@@ -1,14 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        /* Older switch statement before java 12 */
-        int num = 2;
-        switch (num) {
-            case 1:
-                System.out.println("Hello world!");
-                break;
-            default:
-                System.out.println("default");
-                break;
+         /* New switch statement
+          * It allow us the expression version -> , when we use this we don't have to explicitly add break
+          * We can use new switch statement to assign values as well which was not possible with the older version
+          * we can return value as well with the new switch statement
+         */
+
+        String day = "Monday";
+        /* It allow us the expression version -> , when we use this we don't have to explicitly add break
+        this will return It's the weekday
+        */
+        switch (day) {
+            case "Saturday, Sunday" -> System.out.println("It's the weekend");
+            default -> System.out.println("It's the weekday");
         }
+
+        // We can use new switch statement to assign values as well which was not possible with the older version
+        int result;
+        switch (day) {
+            case "Saturday, Sunday" -> result = 0;
+            default -> result = 1;
+        }
+        System.out.println(result);
+
+        // we can return value as well with the new switch statement
+
+        result = switch (day) {
+            case "Saturday, Sunday" -> 0;
+            default -> 1;
+        };
+        System.out.println(result);
     }
 }
