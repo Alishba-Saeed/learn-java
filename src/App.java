03e4b65a7480f7a9f08b080f5b9da653a53b1837
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-08-27 22:20:02 +0500

    String buffer in java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String org = "Alishba";
-        String copy = "Alishba";
-        System.out.println("Org and copy are same: "+ (org == copy)); //true
-        String newOrg = new String("Alishba");
-        System.out.println("newOrg is different: "+ (newOrg == org)); //false
+        StringBuffer buffer = new StringBuffer("");
+        System.out.println(buffer.capacity());
+        System.out.println(buffer.hashCode());
+        buffer.append("Alishba");
+        System.out.println(buffer);
+        buffer.ensureCapacity(20);
+        System.out.println(buffer.capacity());
+        System.out.println(buffer.hashCode());
     }
 }
