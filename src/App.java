c331981a3eb209e076c3d522486cf02b02431945
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-08-27 15:18:16 +0500

    Array basics

--- src/App.java
+++ src/App.java
@@ -1,26 +1,13 @@
 public class App {
-    /*
-     * The methods like main and add will have their own stacks
-     * main method stack will have variables: a, b, obj, obj1
-     *
-     * main
-     * ----------------
-     *| Variable | Value |
-     * ----------------
-     * |    a     |   2   |
-     * |    b     |   3   |
-     * |   obj    | Ref variable to heap  |
-     * |   obj1   |   Ref variable to heap  |
-     * ----------------
-     * We will have a similar stacks for both add methods of calculator class
-     * Heap will contain two parts, one for Calculator class variable and one for method
-     */
     public static void main(String[] args) throws Exception {
-        int a = 2;
-        int b = 3;
-        Calculator obj = new Calculator();
-        Calculator obj1 = new Calculator();
-        System.out.println(obj.add(a, b));
-        System.out.println(obj1.add(3, 2));
+        int nums[] = {3, 4, 5, 6}; //This syntax is called an array initializer
+
+        int num[] = new int[4]; // In this num array will be automatically initilized with 0
+        for (int i=0; i< num.length;i++) {
+            num[i] = i * (num.length + 2);
+        }
+        for (int i=0; i< num.length;i++) {
+            System.out.println(nums[i] +" "+ num[i]);
+        }
     }
 }
