07ca5de00f28dc0f715ea0e4a22fc09b790579c2
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-08-27 23:03:59 +0500

    Static variable, method and block

--- src/App.java
+++ src/App.java
@@ -1,12 +1,31 @@
+class Person {
+    static String specie;
+    String name;
+    int age;
+
+    public Person() {
+        specie = "human";
+        System.out.println("In person constructor");
+    }
+
+    static {
+        System.out.println("In static block");
+    }
+
+    static void show(Person p) {
+        System.out.println("Name: "+p.name+" age: "+p.age+ " "+Person.specie);
+    }
+}
 public class App {
     public static void main(String[] args) throws Exception {
-        StringBuffer buffer = new StringBuffer("");
-        System.out.println(buffer.capacity());
-        System.out.println(buffer.hashCode());
-        buffer.append("Alishba");
-        System.out.println(buffer);
-        buffer.ensureCapacity(20);
-        System.out.println(buffer.capacity());
-        System.out.println(buffer.hashCode());
+        Person p1 = new Person();
+        p1.name = "Alishba";
+        p1.age = 23;
+        Person.show(p1);
+        Person p2 = new Person();
+        Person.specie = "Homosapiens";
+        p2.name = "Sana";
+        p2.age = 26;
+        Person.show(p2);
     }
 }
