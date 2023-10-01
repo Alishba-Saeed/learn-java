983b0ca8bc78ac96a05649311178607f78abfaca
Alishba Saeed <alishbasaeed7109@gmail.com>
2023-10-01 05:20:00 -0700

    Implemented covariant type

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,4 @@
-public class Cat {
+public class Cat extends Animal{
   public String print() {
     System.out.println("Hi I am the cat class");
     return "Cat";
--- src/Dog.java
+++ src/Dog.java
@@ -1,7 +1,6 @@
-public class Dog {
+public class Dog extends Animal{
   public Integer print() {
     System.out.println("Hi I am the dog class");
     return 1;
   }
-
 }
